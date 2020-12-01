79abb6405fbadcfb354a97158926f4d7058494f2
ShinChven ZHANG <shinchven@gmail.com>
2020-12-01 22:30:42 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# torrent-2-magnet
+
+A cli to parse torrent file to magnet url.
+
+
+## install
+
+```bash
+npm link
+```
+
+## usage
+
+```bash
+t2m <your_torrent_file>
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const parseTorrent = require('parse-torrent')
+const fs = require('fs');
+const torrentFile = process.argv[2];
+
+data = parseTorrent(fs.readFileSync(torrentFile));
+const uri = parseTorrent.toMagnetURI(data);
+console.log(uri);
+
